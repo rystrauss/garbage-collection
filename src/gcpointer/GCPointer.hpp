a4a8848a00d609299d9be1c177ab31007fa86988
Ryan Strauss <rystrauss@davidson.edu>
2019-12-19 12:34:01 -0500

    Move function implementations inside class

--- src/gcpointer/GCPointer.hpp
+++ src/gcpointer/GCPointer.hpp
@@ -25,7 +25,14 @@ private:
      *
      * If the data is nullptr, nothing will happen.
      */
-    inline void increment_references() const;
+    inline void increment_references() const {
+        if (data != nullptr) {
+            if (references.find(data) == references.end())
+                references[data] = 0;
+
+            references[data]++;
+        }
+    }
 
     /**
      * Decrements the number of references to the data contained in this smart pointer.
@@ -35,195 +42,130 @@ private:
      *
      * If the data is nullptr, nothing will happen.
      */
-    inline void decrement_references();
+    inline void decrement_references() {
+        if (data != nullptr && --references[data] == 0) {
+            references.erase(data);
+            delete data;
+        }
+    }
 
 public:
 
     // Constructor
-    explicit GCPointer(T *pointer = nullptr);
+    explicit GCPointer(T *pointer = nullptr) : data{pointer} {
+        increment_references();
+    }
 
     // Copy Constructor
-    GCPointer(const GCPointer &gcPointer);
+    GCPointer(const GCPointer &gcPointer) : data{gcPointer.data} {
+        increment_references();
+    }
 
     // Move Constructor
-    GCPointer(GCPointer &&gcPointer) noexcept;
+    GCPointer(GCPointer &&gcPointer) noexcept {
+        decrement_references();
+        data = gcPointer.data;
+        gcPointer.data = nullptr;
+    }
 
     // Destructor
-    ~GCPointer();
+    ~GCPointer() {
+        decrement_references();
+    }
 
     // Copy Assignment
-    GCPointer<T> &operator=(const GCPointer<T> &gcPointer);
+    GCPointer<T> &operator=(const GCPointer<T> &gcPointer) {
+        if (this == &gcPointer)
+            return *this;
 
-    GCPointer<T> &operator=(T *t);
+        decrement_references();
+        data = gcPointer.data;
+        increment_references();
+        return *this;
+    }
+
+    GCPointer<T> &operator=(T *t) {
+        decrement_references();
+        data = t;
+        increment_references();
+        return *this;
+    }
 
     // Move Assignment
-    GCPointer<T> &operator=(GCPointer<T> &&gcPointer) noexcept;
+    GCPointer<T> &operator=(GCPointer<T> &&gcPointer) noexcept {
+        decrement_references();
+        data = gcPointer.data;
+        gcPointer.data = nullptr;
+        return *this;
+    }
 
-    GCPointer<T> operator+(int i) const;
+    GCPointer<T> operator+(int i) const {
+        return new GCPointer<T>{data + i};
+    }
 
-    GCPointer<T> operator-(int i) const;
+    GCPointer<T> operator-(int i) const {
+        return new GCPointer<T>{data - i};
+    }
 
-    GCPointer<T> &operator+=(int i);
+    GCPointer<T> &operator+=(int i) {
+        decrement_references();
+        data += i;
+        increment_references();
+        return *this;
+    }
 
-    GCPointer<T> &operator-=(int i);
+    GCPointer<T> &operator-=(int i) {
+        decrement_references();
+        data -= i;
+        increment_references();
+        return *this;
+    }
 
-    GCPointer<T> &operator++();
+    GCPointer<T> &operator++() {
+        decrement_references();
+        data++;
+        increment_references();
+        return *this;
+    }
 
-    const GCPointer<T> operator++(int);
+    const GCPointer<T> operator++(int) {
+        GCPointer<T> unmodified{*this};
+        data++;
+        increment_references();
+        return unmodified;
+    }
 
-    GCPointer<T> &operator--();
+    GCPointer<T> &operator--() {
+        decrement_references();
+        data--;
+        increment_references();
+        return *this;
+    }
 
-    const GCPointer<T> operator--(int);
+    const GCPointer<T> operator--(int) {
+        GCPointer<T> unmodified{*this};
+        data--;
+        increment_references();
+        return unmodified;
+    }
 
-    T *operator->() const;
+    T *operator->() const {
+        return data;
+    }
 
-    T &operator*() const;
+    T &operator*() const {
+        return *data;
+    }
 
     /**
      * Retrieves the pointer contained in this smart pointer.
      *
      * @return the pointer that this GCPointer wraps around
      */
-    T *pointer();
+    T *pointer() {
+        return data;
+    }
 
 };
-
-template<typename T>
-GCPointer<T>::GCPointer(T *pointer) : data{pointer} {
-    increment_references();
-}
-
-template<typename T>
-GCPointer<T>::GCPointer(const GCPointer<T> &gcPointer) : data{gcPointer.data} {
-    increment_references();
-}
-
-template<typename T>
-GCPointer<T>::GCPointer(GCPointer &&gcPointer) noexcept {
-    decrement_references();
-    data = gcPointer.data;
-    gcPointer.data = nullptr;
-}
-
-template<typename T>
-GCPointer<T>::~GCPointer() {
-    decrement_references();
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator=(const GCPointer<T> &gcPointer) {
-    decrement_references();
-    data = gcPointer.data;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator=(T *t) {
-    decrement_references();
-    data = t;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator=(GCPointer<T> &&gcPointer) noexcept {
-    decrement_references();
-    data = gcPointer.data;
-    gcPointer.data = nullptr;
-    return *this;
-}
-
-template<typename T>
-GCPointer<T> GCPointer<T>::operator+(int i) const {
-    return new GCPointer<T>{data + i};
-}
-
-template<typename T>
-GCPointer<T> GCPointer<T>::operator-(int i) const {
-    return new GCPointer<T>{data - i};
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator+=(int i) {
-    decrement_references();
-    data += i;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator-=(int i) {
-    decrement_references();
-    data -= i;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator++() {
-    decrement_references();
-    data++;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-const GCPointer<T> GCPointer<T>::operator++(int) {
-    GCPointer<T> unmodified{*this};
-    data++;
-    increment_references();
-    return unmodified;
-}
-
-template<typename T>
-GCPointer<T> &GCPointer<T>::operator--() {
-    decrement_references();
-    data--;
-    increment_references();
-    return *this;
-}
-
-template<typename T>
-const GCPointer<T> GCPointer<T>::operator--(int) {
-    GCPointer<T> unmodified{*this};
-    data--;
-    increment_references();
-    return unmodified;
-}
-
-template<typename T>
-T *GCPointer<T>::operator->() const {
-    return data;
-}
-
-template<typename T>
-T &GCPointer<T>::operator*() const {
-    return *data;
-}
-
-template<typename T>
-T *GCPointer<T>::pointer() {
-    return data;
-}
-
-template<typename T>
-inline void GCPointer<T>::increment_references() const {
-    if (data != nullptr) {
-        if (references.find(data) == references.end())
-            references[data] = 0;
-
-        references[data]++;
-    }
-}
-
-template<typename T>
-inline void GCPointer<T>::decrement_references() {
-    if (data != nullptr && --references[data] == 0) {
-        references.erase(data);
-        delete data;
-    }
-}
-
 
 #endif //GCPOINTER_HPP
